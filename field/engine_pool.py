ae7d26f310ecd1e22a3b0ae7cf504f842dde88f2
nlroel <lroel@buaa.edu.cn>
2020-03-31 12:00:21 +0800

    512go

--- field/engine_pool.py
+++ field/engine_pool.py
@@ -10,7 +10,7 @@ class EnginePool:
         # session_names = matlab.engine.find_matlab()
         # self.engines = list(map(MatlabEngine, session_names))
         engines = []
-        for i in range(6):
+        for i in range(4):
             engines.append(matlab.engine.start_matlab("-nodesktop -nojvm -nosplash"))
         self.engines = engines
 
